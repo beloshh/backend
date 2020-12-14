7e8172619c72b01bcf2fc13aeb89a3db2d9cc21f
badanga ishak <badangabello@gmail.com>
2020-12-14 09:52:40 -0800

    Updates

---
+++ precache-manifest.e0f007cbc8a4dc8ab7bf14e01c1f50bc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "394d020cfce22153d55d4c9da491441f",
+    "url": "/backend/index.html"
+  },
+  {
+    "revision": "faf937794d0c2ebdb7a0",
+    "url": "/backend/static/css/2.a2392d5c.chunk.css"
+  },
+  {
+    "revision": "afc3f9435a678904c4ae",
+    "url": "/backend/static/css/main.8e3f80af.chunk.css"
+  },
+  {
+    "revision": "faf937794d0c2ebdb7a0",
+    "url": "/backend/static/js/2.de067efb.chunk.js"
+  },
+  {
+    "revision": "6aeb469c0c20fd3334c86de961c4692c",
+    "url": "/backend/static/js/2.de067efb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "afc3f9435a678904c4ae",
+    "url": "/backend/static/js/main.910b8abe.chunk.js"
+  },
+  {
+    "revision": "889aa8002b293140697c",
+    "url": "/backend/static/js/runtime-main.29b27510.js"
+  },
+  {
+    "revision": "df6601b0961197fd570b44815ac44c68",
+    "url": "/backend/static/media/bg-header-desktop.df6601b0.png"
+  }
+]);
